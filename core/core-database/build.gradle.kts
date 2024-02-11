b400104f03c1577f8d928675b7adb96181927690
dantrapdev <dantrapdev@gmail.com>
2024-02-11 12:44:46 +0300

    Create database module

---
+++ core/core-database/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("cinemania.convention.android.library")
+    alias(libs.plugins.kotlin.ksp)
+}
+
+android.namespace = "com.dantrap.cinemania.fintech.core.database"
+
+dependencies {
+    implementation(libs.converter.gson)
+    implementation(libs.bundles.room)
+    ksp(libs.room.compiler)
+    implementation(libs.koin.android)
+}
