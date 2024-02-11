0a35bed6a49dd32c0d4fa74bcdbc75955a73bc21
dantrapdev <dantrapdev@gmail.com>
2024-02-11 18:25:17 +0300

    Add keep annotation

--- core/core-network/src/main/java/com/dantrap/cinemania/fintech/core/network/model/SearchMovieResponseDto.kt
+++ core/core-network/src/main/java/com/dantrap/cinemania/fintech/core/network/model/SearchMovieResponseDto.kt
@@ -1,7 +1,9 @@
 package com.dantrap.cinemania.fintech.core.network.model
 
+import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 
+@Keep
 data class SearchMovieResponseDto(
     @SerializedName("films")
     val movies: List<MovieDto>,
