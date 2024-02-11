5898a9c39080b8a0eeb0c7700b28b7255bdbd83f
dantrapdev <dantrapdev@gmail.com>
2024-02-11 18:08:26 +0300

    Refactor privacy and terms screen

--- features/feature-settings/src/main/java/com/dantrap/cinemania/fintech/feature/settings/privacy/ui/PrivacyScreen.kt
+++ features/feature-settings/src/main/java/com/dantrap/cinemania/fintech/feature/settings/privacy/ui/PrivacyScreen.kt
@@ -5,21 +5,15 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.dantrap.cinemania.fintech.core.ui.R
-import com.dantrap.cinemania.fintech.core.ui.components.ProgressLottieAnimation
 import com.dantrap.cinemania.fintech.feature.settings.privacy.states.PrivacyEvent
 import com.dantrap.cinemania.fintech.feature.settings.privacy.states.PrivacyState
 import com.google.accompanist.web.LoadingState
@@ -40,15 +34,10 @@ fun PrivacyScreen(
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padding)
+                .padding(top = padding.calculateTopPadding())
         ) {
             when (state.url.isEmpty()) {
-                true -> ProgressLottieAnimation(
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .size(200.dp),
-                    rawAnimId = R.raw.lottie_ball_animation
-                )
+                true -> CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
 
                 else -> PrivacyScreenContent(url = state.url)
             }
@@ -61,15 +50,11 @@ private fun PrivacyScreenContent(url: String) {
     val navigator = rememberWebViewNavigator()
     val state = rememberWebViewState(url = url)
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .windowInsetsPadding(WindowInsets.systemBars)
-    ) {
+    Column(modifier = Modifier.fillMaxSize()) {
         val loadingState = state.loadingState
         if (loadingState is LoadingState.Loading) {
             LinearProgressIndicator(
-                progress = loadingState.progress,
+                progress = { loadingState.progress },
                 modifier = Modifier.fillMaxWidth(),
             )
         }
